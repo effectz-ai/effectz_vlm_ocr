f5f9e0c2d8659f64cf574b106d020c92c4dd856c
navindu-sachintha <navindu.social@gmail.com>
2024-11-28 15:26:12 +0530

    Added files

---
+++ frontend/app/layout.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: 'Home',
+    description: 'Welcome to Next.js',
+  }
+
+export default function RootLayout({
+    children,
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
